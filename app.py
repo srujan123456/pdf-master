b624b872f99eb5e8eeaa30f3178669b16362e38c
SrujanDhoni123 <srujanpogathota1236@gmail.com>
2026-02-21 22:53:33 +0530

    Fixed port for render

--- app.py
+++ app.py
@@ -358,4 +358,5 @@ def pdf_info():
     return render_template('info.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
